87e6f4501d1d3361518ecbe5262b03b5868db8fd
r0mj4k <126695989+r0mj4k@users.noreply.github.com>
2025-03-18 19:15:56 +0100

    BookBox-API: (#14, #15)

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,9 +2,33 @@ const Book = require('../models/Book');
 
 exports.getAllBooks = async (req, res) => {
     try {
-        const book = await Book.find();
-        res.json(book);
+        const books = await Book.find();
+        res.json(books);
     } catch (error) {
         res.status(500).json({ error: 'An error occured while fetching books'});
+    }
+};
+
+exports.getBookById = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        if(!book){
+            return res.status(404).json({message: "Book not found"});
+        }
+        res.json(book);
+    } catch (error){
+        res.status(500).json({ message: error.message});
+    }
+};
+
+exports.deleteBook = async (req, res) => {
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id);
+        if(!book){
+            return res.status(404).json({message: "Book not found"});
+        }
+        res.json({message: "Book deleted successfully"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
 };
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
+//endpoint get all books
 router.get('/books', bookController.getAllBooks);
+
+//endpoint get a book
+router.get('/books/:id', bookController.getBookById);
+
+//endpoint delete a book
+router.delete('/books/:id', bookController.deleteBook);
 
 module.exports = router;
